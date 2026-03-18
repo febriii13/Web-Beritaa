a66f39f8bfb9ae256463a526e304aac4394bb13f
febryan saputra <saputrafebryan77@gmail.com>
2026-03-18 23:50:22 +0700

    Update dasboradmin.php

--- dasboradmin.php
+++ dasboradmin.php
@@ -283,4 +283,11 @@ if(!isset($_SESSION['admin'])){
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
+<footer>
+    <div class="bg-dark text-white text-center p-3">
+        &copy; 2024 BeritaKu. All rights reserved.
+    </div>
+</footer>
+
 </html>
